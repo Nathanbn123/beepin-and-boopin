40cfb2d3ff772e7581f62fbd2e2dea88f9fb09c7
Nathan Nielson <nathanbn123@gmail.com>
2019-01-25 11:15:50 -0800

    get carried away and forget to make commits, but will add a non-functioning function to translate inputs

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,18 @@
 // var numArray = input.map(function(number) {
 //     return
 
+function translate(input) {
+  for (var j = 0; j < input.length; j += 1){
+    if (input[j] == 3) {
+      return "I'm sorry, Dave. I can't do that."
+    } else if (input[j] == 2) {
+      return "Boop!";
+    } else if (input[j] == 1) {
+      return "Beep!";
+    }
+  }
+}
+
 
 
 
@@ -21,8 +33,9 @@ $(document).ready(function() {
   $("#roboForm").submit(function(event) {
     event.preventDefault();
     var roboInput = parseInt($('.robot').val());
-    for (var i = 0; i <= roboInput; i += 1) {
-      console.log(i);
+    for (var roboOut = 0; roboOut <= roboInput; roboOut += 1) {
+      translate(roboOut);
+
     }
 
 
