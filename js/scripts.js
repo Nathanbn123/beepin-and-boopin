52b9fffae05c6c230ed6c11b7cab01e7b523e6f6
Nathan Nielson <nathanbn123@gmail.com>
2019-01-30 22:06:19 -0800

    comment out old code

--- js/scripts.js
+++ js/scripts.js
@@ -73,28 +73,28 @@ $(document).ready(function() {
    /**********************************************************************
    OLD CODE
    *****************/
-   function translate(input) {
-  if (input.includes('3')) {
-     return "I'm sorry, Dave. I can't do that.";
-  } else if (input.includes('2')) {
-      return "Boop!";
-  } else if (input.includes('1')) {
-      return "Beep!";
-  } else {
-      return input
-  }
-}
-
-$(document).ready(function() {
-  $("#roboForm").submit(function(event) {
-    event.preventDefault();
-    $( ".output .card-text" ).empty();
-    var roboInput = parseInt($('.robot').val());
-    for (var roboOut = 0; roboOut <= roboInput; roboOut += 1) {
-    var roboString = roboOut.toString();
-      var translated = translate(roboString);
-      $('.output .card-text').append(' '+ '"' + translated + '"' + ',');
-
-    }
-  });
-});
+//    function translate(input) {
+//   if (input.includes('3')) {
+//      return "I'm sorry, Dave. I can't do that.";
+//   } else if (input.includes('2')) {
+//       return "Boop!";
+//   } else if (input.includes('1')) {
+//       return "Beep!";
+//   } else {
+//       return input
+//   }
+// }
+//
+// $(document).ready(function() {
+//   $("#roboForm").submit(function(event) {
+//     event.preventDefault();
+//     $( ".output .card-text" ).empty();
+//     var roboInput = parseInt($('.robot').val());
+//     for (var roboOut = 0; roboOut <= roboInput; roboOut += 1) {
+//     var roboString = roboOut.toString();
+//       var translated = translate(roboString);
+//       $('.output .card-text').append(' '+ '"' + translated + '"' + ',');
+//
+//     }
+//   });
+// });
